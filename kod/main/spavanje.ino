4a72e9d9967a900b775888ff6c81b42dbce3d58f
AnAmusedAnt <132146694+Darmarin@users.noreply.github.com>
2026-02-28 17:11:19 +0100

    Update spavanje.ino

--- kod/main/spavanje.ino
+++ kod/main/spavanje.ino
@@ -6,20 +6,21 @@ bool asleep=false;
 void spavanje_setup() {
   pinMode(BUT_DOWN_PIN, INPUT_PULLUP);
 
+  esp_sleep_enable_ext0_wakeup((gpio_num_t)BUT_DOWN_PIN, 0);
+  gpio_pullup_en((gpio_num_t)BUT_DOWN_PIN);
+  gpio_pulldown_dis((gpio_num_t)BUT_DOWN_PIN);
+  
   wake_millis = millis();
+  Serial.flush();
+  esp_light_sleep_start();
 }
 
 void spavanje_loop() {
-  if (!asleep) {
-    cur_millis = millis();
-    if (cur_millis - wake_millis >= 30*1000) {
-      asleep = true;
-    }
-  }
-  else {
-    if (!digitalRead(BUT_DOWN_PIN)) {
-      asleep = false;
-      wake_millis = millis();
-    }
+  Serial.print('a');
+  Serial.println();
+  cur_millis = millis();
+  if (cur_millis - wake_millis >= 30*1000) {
+    asleep = true;
+    esp_light_sleep_start();
   }
 }
